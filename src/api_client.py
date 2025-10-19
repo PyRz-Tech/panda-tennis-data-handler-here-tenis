2d073a9bdfbba9c10680afae57a8ce089c50b7c3
PyRz-Tech <pyrz.tech@gmail.com>
2025-10-20 00:59:58 +0330

    refactor(api_client): change structure to modular and remove hardcoding

--- src/api_client.py
+++ src/api_client.py
@@ -1,40 +1,15 @@
-import requests
-import json
-from config.config import get_api_key
-import pandas as pd
-class GetInfoApi():
-	def __init__(self):
-		self.api_key = get_api_key()
-		self.url = f"https://api.api-tennis.com/tennis/?method=get_tournaments&APIkey={self.api_key}"
-	def response_from_site_tenis_api(self):
-		try:
-			response = requests.get(self.url)
-			if response.status_code == 200:
-				return True, response.json()
-			else:
-				return False, response.status_code
+from config.config import API_KEY
+from src.utils import make_api_request
+import logging
 
-		except requests.exceptions.RequestException as e:
-			print(f"Error occured: {e}")
-			return False, str(e)
+logger = logging.getLogger(__name__)
+logging.basicConfig(encoding='utf-8', level=logging.INFO)
 
-	def get_json_response(self):
-			success, data = self.response_from_site_tenis_api()
-			if success:
-				return data
-			else:
-				print(f"Failed to fetch data: {data}")
-				return None
+class GetApiClient:
+    def __init__(self):
+        self.api_key = API_KEY
+        self.url = f"https://api.api-tennis.com/tennis/?method=get_tournaments&APIkey={self.api_key}"
 
-	def save_to_dataframe(self):
-		data = self.get_json_response()
-		if data and data.get("success") == 1:
-			try:
-				result = data.get("result", [])
-				df = pd.DataFrame(result)
-				df.to_csv("../tenis_tournaments.csv", index=False)
-				return df
-			except Exception as e:
-				print(f"Error converting to DataFrame: {e}")
-				return None
-		return None
+    def fetch_tournament_data(self):
+        logger.info("try to fetch tournament data")
+        return make_api_request(self.url)
